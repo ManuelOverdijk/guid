9fd1c1a6a06187623e3dc89f8a816ca9975c32ea
jetsekoopmans <jetsekoopmans@hotmail.com>
2014-01-09 12:50:13 +0100

    rails new epo_login

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EpoLogin::Application.config.secret_key_base = '7a5c4113ce479e7b1ae0e69fa88458a169541a57aa7133be8f520339a9e72d40fdc63ce5c261ae6b0dce588daf71fb4efd7a43758a000ca5e2e0cf7a96595265'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EpoLogin::Application.config.session_store :cookie_store, key: '_epo_login_session'
