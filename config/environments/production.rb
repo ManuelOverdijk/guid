6f87604e6b59e7c03204581c936f1042118691e2
Koen ten Katen <koen.tenkaten@student.uva.nl>
2014-01-14 16:35:03 +0100

    remove master from production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 EpoLogin::Application.configure do
 
   # Settings specified here will take precedence over those in config/application.rb.
-  master
   # Code is not reloaded between requests.
   config.cache_classes = true
 
