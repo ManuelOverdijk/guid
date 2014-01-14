9e1b91d9fddd77db9f3c22081bcab644df2108c2
Manuel Overdijk -M <manuel.overdijk@gmail.com>
2014-01-14 15:15:40 +0100

    date picker working, still lots of bugs

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -5,22 +5,31 @@ class TokensController < ApplicationController
 	def create
 		privatehash = SecureRandom.uuid()
 		puts params[:token][:timevalid]
-		if params[:token][:timevalid] == "1"
-			@timevalid = 7*24
-		end
-		if params[:token][:timevalid] == "2"
-			@timevalid = 2*7*24
-		else params[:token][:timevalid] == "3"
-			@timevalid = 4*7*24
-		end
 
-		@token = Token.new(timevalid: @timevalid, token: privatehash, 
-                       user_id: session[:user_id])
+		#current date
+		puts DateTime.now.to_date
+		if Date.parse(params[:token][:timevalid]) < Date.today
+			#Date picked before today, raise error
 
-		if @token.save!
-			redirect_to private_path(:id => privatehash)
-		else
-			redirect_to generate_path
-		end
+		
+		# if params[:token][:timevalid] == "1"
+		# 	@timevalid = 7*24
+		# end
+		# if params[:token][:timevalid] == "2"
+		# 	@timevalid = 2*7*24
+		# else params[:token][:timevalid] == "3"
+		# 	@timevalid = 4*7*24
+		# end
+
+
+
+		# @token = Token.new(timevalid: @timevalid, token: privatehash, 
+  #                      user_id: session[:user_id])
+
+		# if @token.save!
+		# 	redirect_to private_path(:id => privatehash)
+		# else
+		# 	redirect_to generate_path
+		# end
 	end
 end
