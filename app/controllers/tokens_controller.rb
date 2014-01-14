65410523cef7b00a02b5a0e62283edb9a8f4013e
Manuel Overdijk -M <manuel.overdijk@gmail.com>
2014-01-14 13:24:38 +0100

    drop down menu working, still bug fixing to do

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -4,8 +4,18 @@ class TokensController < ApplicationController
 
 	def create
 		privatehash = SecureRandom.uuid()
-		@token = Token.new(timevalid: params[:token][:timevalid], token: privatehash, 
-                        user_id: session[:user_id])
+		puts params[:token][:timevalid]
+		if params[:token][:timevalid] == "1"
+			@timevalid = 7*24
+		end
+		if params[:token][:timevalid] == "2"
+			@timevalid = 2*7*24
+		else params[:token][:timevalid] == "3"
+			@timevalid = 4*7*24
+		end
+
+		@token = Token.new(timevalid: @timevalid, token: privatehash, 
+                       user_id: session[:user_id])
 
 		if @token.save!
 			redirect_to private_path(:id => @token.token)
