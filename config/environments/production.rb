367632b87f492f31a3c5e4408aa6078a1fad252d
Koen ten Katen <koen.tenkaten@student.uva.nl>
2014-01-14 16:18:32 +0100

    final adjustment for today

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Guid::Application.configure do
 
   # Settings specified here will take precedence over those in config/application.rb.
-
+  B-master
   # Code is not reloaded between requests.
   config.cache_classes = true
 
