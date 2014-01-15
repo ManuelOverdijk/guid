5c989a53db6b18e6e9410b7836f913640ce0aa05
Manuel Overdijk -M <manuel.overdijk@gmail.com>
2014-01-15 13:31:51 +0100

    date working, timepicker not

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require jquery
 //= require bootstrap-datetimepicker
-//= require bootstrap
+//= require_tree .
+
+
